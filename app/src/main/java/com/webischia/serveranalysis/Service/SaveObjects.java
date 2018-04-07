dcae463aa9d3c06bec106e75c9b6cba5c365605d
ffahri <ffahri@users.noreply.github.com>
2018-04-07 20:52:42 +0300

    SaveObjects service and impl added

---
+++ app/src/main/java/com/webischia/serveranalysis/Service/SaveObjects.java
@@ -0,0 +1,7 @@
+package com.webischia.serveranalysis.Service;
+
+//Android hafızasına burada tanımladığımız metotlar ile yazacağız.
+public interface SaveObjects {
+
+    public void SaveGraphics();
+}
---
+++ app/src/main/java/com/webischia/serveranalysis/Service/SaveObjectsImpl.java
@@ -0,0 +1,10 @@
+package com.webischia.serveranalysis.Service;
+
+// Dosyaya buradaki metotlardan yazdırıyoruz.
+
+public class SaveObjectsImpl implements SaveObjects {
+    @Override
+    public void SaveGraphics() {
+
+    }
+}
