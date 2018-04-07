385d66d76cbc38433e29b17deb23edd7bd6e45b1
ffahri <ffahri@users.noreply.github.com>
2018-04-07 20:54:57 +0300

    Created loginCheck method

--- app/src/main/java/com/webischia/serveranalysis/Service/LoginControl.java
+++ app/src/main/java/com/webischia/serveranalysis/Service/LoginControl.java
@@ -3,4 +3,5 @@ package com.webischia.serveranalysis.Service;
 //Login kontrol metotları tanımlanacak.
 
 public interface LoginControl {
+    public String loginCheck(String username,String password);
 }
--- app/src/main/java/com/webischia/serveranalysis/Service/LoginControlImpl.java
+++ app/src/main/java/com/webischia/serveranalysis/Service/LoginControlImpl.java
@@ -3,4 +3,14 @@ package com.webischia.serveranalysis.Service;
 //Login control metotları bulunacak.
 
 public class LoginControlImpl implements LoginControl {
+    @Override
+    public String loginCheck(String username, String password) {
+        //yanlış şifre ise hata ver ve null dön
+
+        //401 - 403 hatası alırsan hata ver diyalog çıkar
+
+        //doğru ise http 200
+        //token döndür
+        return null;
+    }
 }
