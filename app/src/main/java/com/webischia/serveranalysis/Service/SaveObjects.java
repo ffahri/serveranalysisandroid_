8d02a9f5825fbd311c1ecc26d481480490d4b907
ffahri <ffahri@users.noreply.github.com>
2018-04-07 20:57:50 +0300

    Created loadGraphics method

--- app/src/main/java/com/webischia/serveranalysis/Service/SaveObjects.java
+++ app/src/main/java/com/webischia/serveranalysis/Service/SaveObjects.java
@@ -1,7 +1,11 @@
 package com.webischia.serveranalysis.Service;
 
+import com.webischia.serveranalysis.Models.Graphic;
+
 //Android hafızasına burada tanımladığımız metotlar ile yazacağız.
 public interface SaveObjects {
 
-    public void SaveGraphics();
+    public void saveGraphics();
+
+    public Graphic loadGraphics(String name);
 }
--- app/src/main/java/com/webischia/serveranalysis/Service/SaveObjectsImpl.java
+++ app/src/main/java/com/webischia/serveranalysis/Service/SaveObjectsImpl.java
@@ -2,9 +2,18 @@ package com.webischia.serveranalysis.Service;
 
 // Dosyaya buradaki metotlardan yazdırıyoruz.
 
+import com.webischia.serveranalysis.Models.Graphic;
+
 public class SaveObjectsImpl implements SaveObjects {
     @Override
-    public void SaveGraphics() {
+    public void saveGraphics() {
+            //Java serialization ile yaz
+    }
 
+    @Override
+    public Graphic loadGraphics(String name) {
+            //Java deserialization ile oku graphic nesnesine aç ve döndür
+
+        return null;
     }
 }
