54617b9e57ccf31464515ef1360858b0c31cd79b
ffahri <ffahri@users.noreply.github.com>
2018-04-08 22:11:01 +0300

    Implementing doQuery method

--- app/src/main/java/com/webischia/serveranalysis/Service/QueryService.java
+++ app/src/main/java/com/webischia/serveranalysis/Service/QueryService.java
@@ -3,4 +3,6 @@ package com.webischia.serveranalysis.Service;
 //Server ile android arasındaki querylerin çağırılacağı metotların belirtilidiği yer
 public interface QueryService {
 
+    void doQuery(String query,String token);
+
 }
--- app/src/main/java/com/webischia/serveranalysis/Service/QueryServiceImpl.java
+++ app/src/main/java/com/webischia/serveranalysis/Service/QueryServiceImpl.java
@@ -1,8 +1,26 @@
 package com.webischia.serveranalysis.Service;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.data.Entry;
 import com.webischia.serveranalysis.Controls.QueryControl;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**Query metotlarının yazıldığı ve güvenli bir şekilde servera iletilip rest cevabı
 olan json dosyasının parse edildiği sınıf
@@ -15,5 +33,84 @@ public class QueryServiceImpl implements QueryService {
     public QueryServiceImpl(QueryControl queryControl, Context context) {
         this.queryControl = queryControl;
         this.context = context;
+    }
+
+    @Override
+    public void doQuery(String query, String token) {
+        try {
+            final RequestQueue queue = Volley.newRequestQueue(this);  // this = context
+
+            String url = "http://192.168.122.160:9090/api/v1/query?query=node_cpu{mode=\"idle\"}[1m]";
+            StringRequest postRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // response
+                            Log.d("Response", response);
+
+                            try {
+                                ArrayList<Entry> yValues = new ArrayList<>();
+                                Intent i;
+                                JSONObject jsonObj = new JSONObject(response);
+                                JSONObject jsonObj2 = jsonObj.getJSONObject("data");
+                                JSONArray jsonArr = jsonObj2.getJSONArray("result");
+                                JSONObject jsonArr2 = jsonArr.getJSONObject(0);
+                                JSONArray jsonArr3 = jsonArr2.getJSONArray("values");
+                                for(int ii = 0 ; ii < 5 ; ii++)
+                                {
+                                    JSONArray js3 = jsonArr3.getJSONArray(ii);
+                                    Log.d("js3", js3.toString());
+
+                                    String axes_x = js3.getString(1);
+                                    Date timestamp = new Date(js3.getLong(0)*1000L);
+                                    Float x = Float.parseFloat(axes_x);
+                                    // x = x/100000000;
+                                    yValues.add(new Entry((float)timestamp.getSeconds(),x)); //x 0 y 60 olsun f de float f si
+
+                                }
+//
+
+
+
+                                ////////////////// GRAFIK
+
+                            }
+                            catch (Exception e)
+                            {
+                                Log.d("hata", e.getMessage());
+
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // error
+                            Log.d("Error.Response","error");
+
+
+                        }
+                    }
+            ) {
+
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    String token = getIntent().getExtras().getString("token");
+                    if(token != null) {
+                        params.put("Authorization", "Basic VXNlcjoxMjM0");
+
+                        return params;
+                    }
+                    else
+                        return null;
+                }
+            };
+            queue.add(postRequest);
+        }
+        catch(Exception e)
+        {
+        }
     }
 }
