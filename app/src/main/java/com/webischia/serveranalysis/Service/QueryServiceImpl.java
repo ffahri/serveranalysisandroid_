82ee9f7588dd9f90cd213ddbecf4d3914b4e9f08
ffahri <ffahri@users.noreply.github.com>
2018-04-08 21:59:52 +0300

    Implementing graphic controller

--- app/src/main/java/com/webischia/serveranalysis/Service/QueryServiceImpl.java
+++ app/src/main/java/com/webischia/serveranalysis/Service/QueryServiceImpl.java
@@ -1,8 +1,19 @@
 package com.webischia.serveranalysis.Service;
 
+import android.content.Context;
+
+import com.webischia.serveranalysis.Controls.QueryControl;
+
 /**Query metotlarının yazıldığı ve güvenli bir şekilde servera iletilip rest cevabı
 olan json dosyasının parse edildiği sınıf
  **/
 
 public class QueryServiceImpl implements QueryService {
+    QueryControl queryControl;
+    Context context;
+
+    public QueryServiceImpl(QueryControl queryControl, Context context) {
+        this.queryControl = queryControl;
+        this.context = context;
+    }
 }
--- app/src/main/java/com/webischia/serveranalysis/ShowGraphic.java
+++ app/src/main/java/com/webischia/serveranalysis/ShowGraphic.java
@@ -1,22 +1,49 @@
 package com.webischia.serveranalysis;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.webischia.serveranalysis.Controls.QueryControl;
+import com.webischia.serveranalysis.Service.QueryService;
+import com.webischia.serveranalysis.Service.QueryServiceImpl;
 
 import java.util.ArrayList;
 
 public class ShowGraphic extends AppCompatActivity implements QueryControl{
-
+    QueryControl queryControl;
+    QueryService queryService;
+    LineChart linechart1; //xml den grafik ekranını çıktı aldık
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_graphic);
+        queryControl = new ShowGraphic();
+        queryService = new QueryServiceImpl(queryControl,this);
+        //put extra içine graph koyup query yaptırırız burada
     }
 
     @Override
     public void successQuery(ArrayList list) {
+        //diğer kısımlarda hata var deneyemedik UNIT TEST de yazmadım
+
+        linechart1 = (LineChart) findViewById(R.id.linechart); //xml den java classına çağırdık
+
+        //yValues.add(new Entry((float)timestamp.getMinutes(),x)); //x 0 y 60 olsun f de float f si
+        LineDataSet set1 = new LineDataSet(list,"CPU KULLANIMI");// sol altta yazan yazı
+
+        set1.setFillAlpha(110);
+        set1.setColor(Color.RED);// çizgi rengi
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);// çizginin oluştuğu kısım heralde tam kontrol etmedim
+
+        LineData data = new LineData(dataSets);
+        linechart1.setData(data); // programa ekliyor
 
     }
 }
