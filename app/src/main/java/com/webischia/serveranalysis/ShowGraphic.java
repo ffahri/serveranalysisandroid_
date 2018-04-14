be749ca356f18c46d52bdad333b75aa967300260
ffahri <ffahri@users.noreply.github.com>
2018-04-14 17:23:34 +0300

    Make volley request inside activity

--- app/src/main/java/com/webischia/serveranalysis/ShowGraphic.java
+++ app/src/main/java/com/webischia/serveranalysis/ShowGraphic.java
@@ -8,8 +8,17 @@ import android.graphics.Color;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -20,12 +29,16 @@ import com.webischia.serveranalysis.Models.Graphic;
 import com.webischia.serveranalysis.Service.QueryService;
 import com.webischia.serveranalysis.Service.QueryServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class ShowGraphic extends AppCompatActivity implements QueryControl{
-    QueryControl queryControl;
-    QueryService queryService;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ShowGraphic extends AppCompatActivity{
     LineChart linechart1; //xml den grafik ekranını çıktı aldık
     Graphic graphic;
     Intent i;
@@ -40,13 +53,8 @@ public class ShowGraphic extends AppCompatActivity implements QueryControl{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_graphic);
-        queryControl = new ShowGraphic();
-        queryService = new QueryServiceImpl(queryControl,this);
-        linechart1 = (LineChart) findViewById(R.id.linechart); //xml den java classına çağırdık
-        //startAlarim();
         ArrayList<Entry> yValues = null ;
         ArrayList k = getIntent().getParcelableArrayListExtra("graphic");
-        String token = getIntent().getExtras().getString("token");
         if(k == null)
         {
             //getIntent().getExtras().getString("graphName");
@@ -57,10 +65,13 @@ public class ShowGraphic extends AppCompatActivity implements QueryControl{
 
         if(getIntent().getParcelableArrayListExtra("values") != null)
             yValues = getIntent().getParcelableArrayListExtra("values");
+        editGraph(yValues);
 
-        //graifk utku
-        //put extra içine graph koyup query yaptırırız burada
 
+    }
+    private void editGraph(ArrayList yValues)
+    {
+        linechart1 = (LineChart) findViewById(R.id.linechart); //xml den java classına çağırdık
         linechart1.setDragEnabled(true);
         linechart1.setScaleEnabled(false);
 
@@ -75,12 +86,96 @@ public class ShowGraphic extends AppCompatActivity implements QueryControl{
 
         LineData data = new LineData(dataSets);
         linechart1.setData(data); // programa ekliyor
-
     }
-
     public void refresh(View view)
     {
-        //queryService.refresh();
+
+        ArrayList k = this.getIntent().getParcelableArrayListExtra("graphic");
+        if(k == null)
+        {
+            //getIntent().getExtras().getString("graphName");
+            Log.d("kkkk","K NULL");
+
+        }
+        else
+            graphic = (Graphic)k.get(0);
+        final String token = getIntent().getExtras().getString("token");
+        try {
+            final RequestQueue queue = Volley.newRequestQueue(this);  // this = context
+
+            String url = "https://java.webischia.com/api/v1/metric/"+graphic.httpForm();
+            Log.d("query_url",url);
+            StringRequest postRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // response
+                            Log.d("Response", response);
+                            ArrayList<Entry> yValues = new ArrayList<>();
+
+
+                            try {
+
+                                JSONObject jsonObj = new JSONObject(response);
+                                JSONObject jsonObj2 = jsonObj.getJSONObject("data");
+                                JSONArray jsonArr = jsonObj2.getJSONArray("result");
+                                JSONObject jsonArr2 = jsonArr.getJSONObject(0);
+                                JSONArray jsonArr3 = jsonArr2.getJSONArray("values");
+                                for(int ii = 0 ; ii < 12 ; ii++)
+                                {
+                                    JSONArray js3 = jsonArr3.getJSONArray(ii);
+                                    Log.d("js3", js3.toString());
+
+                                    String axes_x = js3.getString(1);
+                                    Date timestamp = new Date(js3.getLong(0)*1000L);
+                                    Float x = Float.parseFloat(axes_x);
+                                    // x = x/100000000;
+                                    yValues.add(new Entry((float)timestamp.getSeconds(),x)); //x 0 y 60 olsun f de float f si
+                                    Toast.makeText(ShowGraphic.this,"Refreshed",Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                ////////////////// GRAFIK
+                                editGraph(yValues);
+                            }
+                            catch (Exception e)
+                            {
+                                Log.d("Query_ERROR", e.getMessage());
+
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // error
+                            Log.d("Error.Response","error");
+
+
+                        }
+                    }
+            ) {
+
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    if(token != null) {
+                        params.put("Authorization", "Bearer "+token);
+
+                        return params;
+                    }
+                    else
+                        return null;
+                }
+            };
+            queue.add(postRequest);
+        }
+        catch(Exception e)
+        {
+            Log.d("Error?",e.getMessage());
+
+        }
     }
     //alarm
     private void startAlarm() {
@@ -94,8 +189,4 @@ public class ShowGraphic extends AppCompatActivity implements QueryControl{
 
     }
 
-    @Override
-    public void successQuery(ArrayList list,Context context) {
-
-    }
 }
